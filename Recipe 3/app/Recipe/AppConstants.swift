663a3411cbe9b4ab1b84a5bdf0c66d862fb09301
cowboytrix <cowboytrix@yahoo.com>
2018-09-14 22:01:50 -0700

    Input the first paragraph of XML content.

---
+++ Recipe 3/app/Recipe/AppConstants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  Recipe
+//
+//  Copyright © 2015 dmbTEAM. All rights reserved.
+//
+
+import Foundation
+
+let EnableAds = true
+
+let FacebookUrl = "http://facebook.com"
+
+let TwitterUrl = "http://twitter.com"
+
+let AppStoreUrl = "http://store.apple.com"
+
+// Place here URL, if you want to use web hosted XML
+// Leave empty, if you want to use the local one: Supporting Files/recipes_settings.xml
+let RemoteXMLUrl = ""
+
+// AdMob Ads ID, the below one is a test ID
+let adMobAdUnitID = "ca-app-pub-3940256099942544/6300978111"
+
+// Push Notifications (OneSignal) Key
+let OneSignalAppId = ""
